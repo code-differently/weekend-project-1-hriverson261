19be6a94636314398439b68c5caaef635ad4ad06
Henrrietta <hriverson261@gmail.com>
2026-02-06 15:32:09 -0500

    Created scanner for customer input for name, budget, and coupon code

--- src/main/java/org/codedifferently/Main.java
+++ src/main/java/org/codedifferently/Main.java
@@ -1,11 +1,23 @@
 package org.codedifferently;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter full name: ");
+        String custName = input.nextLine();
+
+        System.out.println("Enter your budget");
+        double budget = input.nextDouble();
+
+        System.out.println("Enter coupon code: ");
+        String couponCode = input.nextLine();
+
+        input.close();
 
     }
 
